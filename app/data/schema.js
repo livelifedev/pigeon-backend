3150db3c67852508c75de9698e7034be37c38a7d
John <johnrubio93@gmail.com>
2020-04-03 00:21:59 +1100

    fixing up gql schema

--- app/data/schema.js
+++ app/data/schema.js
@@ -4,29 +4,69 @@ const { makeExecutableSchema } = require("graphql-tools");
 
 // Define our schema using the GraphQL schema language
 const typeDefs = `
-  type User {
-    id: Int!
-    username: String!
-    email: String!
-    posts: [Post]
-  }
-  type Post {
-    id: Int!
-    title: String!
-    slug: String!
-    content: String!
-    user: User!
-  }
-  type Query {
-  allUsers: [User]
-  fetchUser(id: Int!): User
-  allPosts: [Post]
-  fetchPost(id: Int!): Post
+schema {
+  query: Query
+  mutation: Mutation
+}
+
+# root query for Guild of Pigeon Breeders app
+type Query {
+  # an individual user
+  user(id: Int!): User
+  # list of all pigeons
+  pigeons: [Pigeon]
+  # an individual pigeon
+  pigeon(id: Int!): Pigeon
+  # list of all elements
+  elements: [Element]
+  # list of all subBreeds
+  subBreeds: [SubBreed]
 }
 type Mutation {
   login (email: String!, password: String!): String
   createUser (username: String!, email: String!, password: String!): User
   addPost (title: String!, content: String!): Post
+}
+type User {
+  id: Int!
+  breederName: String!
+  rank: String!
+  email: String!
+}
+type Pigeon {
+  id: Int!
+  owner: User!
+  name: String!
+  flock: String!
+  gender: String!
+  region: String!
+  primaryBreed: String!
+  subBreed: String!
+  element: String!
+  dob: Int!
+  bio: String!
+  growth: Int!
+  lifeStage: LifeStage!
+  health: String!
+  appetite: Int!
+  feedSchedule: String!
+  lastFed: Int!
+}
+type Element {
+  id: Int!
+  name: String!
+  description: String!
+}
+type LifeStage {
+  id: Int!
+  stage: Int!
+  name: String!
+  description: String!
+}
+type SubBreed {
+  id: Int!
+  name: String!
+  description: String!
 }
 `;
 
