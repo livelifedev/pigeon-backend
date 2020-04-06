b6de59b5add298e3bbf61d7e6f54fb16ed4d233e
John <johnrubio93@gmail.com>
2020-04-06 13:28:48 +1000

    deploy config

--- config/database.js
+++ config/database.js
@@ -6,6 +6,11 @@ const Env = use("Env");
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use("Helpers");
 
+const Url = require("url-parse");
+const { hostname, port, username, password, pathname } = new Url(
+  Env.get("DATABASE_URL")
+);
+
 module.exports = {
   /*
   |--------------------------------------------------------------------------
@@ -75,11 +80,11 @@ module.exports = {
   pg: {
     client: "pg",
     connection: {
-      host: Env.get("DB_HOST", "localhost"),
-      port: Env.get("DB_PORT", ""),
-      user: Env.get("DB_USER", "postgres"),
-      password: Env.get("DB_PASSWORD", ""),
-      database: Env.get("DB_DATABASE", "pigeonguild")
+      host: Env.get("DB_HOST", hostname),
+      port: Env.get("DB_PORT", port),
+      user: Env.get("DB_USER", username),
+      password: Env.get("DB_PASSWORD", password),
+      database: Env.get("DB_DATABASE", pathname.substr(1))
     },
     debug: Env.get("DB_DEBUG", true)
   }
